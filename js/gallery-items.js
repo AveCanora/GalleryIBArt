8e7d151540f07bc613e0144819ba3bcd51b04e1e
Natalya Taranenko <go.avecanora@gmail.com>
2021-02-21 00:31:39 +0200

    UPDATE

---
+++ js/gallery-items.js
@@ -0,0 +1,77 @@
+export default [
+  {
+    preview: "../img/gallery/1.jpg",
+    original: "../img/gallery/1.jpg",
+    description: "",
+  },
+  {
+    preview: "../img/gallery/2.jpg",
+    original: "../img/gallery/2.jpg",
+    description: "",
+  },
+  {
+    preview: "../img/gallery/3.jpg",
+    original: "../img/gallery/3.jpg",
+    description: "",
+  },
+  {
+    preview: "../img/gallery/4.jpg",
+    original: "../img/gallery/4.jpg",
+    description: "",
+  },
+  {
+    preview: "../img/gallery/5.jpg",
+    original: "../img/gallery/5.jpg",
+    description: "",
+  },
+  {
+    preview: "../img/gallery/6.jpg",
+    original: "../img/gallery/6.jpg",
+    description: "",
+  },
+  {
+    preview: "../img/gallery/7.jpg",
+    original: "../img/gallery/7.jpg",
+    description: "",
+  },
+  {
+    preview: "../img/gallery/8.jpg",
+    original: "../img/gallery/8.jpg",
+    description: "",
+  },
+  {
+    preview: "../img/gallery/9.jpg",
+    original: "../img/gallery/9.jpg",
+    description: "",
+  },
+  {
+    preview: "../img/gallery/10.jpg",
+    original: "../img/gallery/10.jpg",
+    description: "",
+  },
+  {
+    preview: "../img/gallery/11.jpg",
+    original: "../img/gallery/11.jpg",
+    description: "",
+  },
+  {
+    preview: "../img/gallery/12.jpg",
+    original: "../img/gallery/12.jpg",
+    description: "",
+  },
+  {
+    preview: "../img/gallery/13.jpg",
+    original: "../img/gallery/13.jpg",
+    description: "",
+  },
+  {
+    preview: "../img/gallery/14.jpg",
+    original: "../img/gallery/14.jpg",
+    description: "",
+  },
+  {
+    preview: "../img/gallery/15.jpg",
+    original: "../img/gallery/15.jpg",
+    description: "",
+  },
+];
---
+++ js/modul.js
@@ -0,0 +1,95 @@
+// for(var i=0; i< 100;i++)
+// {
+// document.write('<li><a href="images/gallery/' + i + '.jpg"><img src="images/gallery/' + i + '.jpg" ></a></li>');
+// }
+import imagesObj from "./gallery-items.js";
+//переменные:  массив ссылок на картинки и индекс
+//для релизации задачи пролистывания изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
+var collectionImg = imagesObj.map((img) => img.original);
+var currentIndexImg = 0;
+//переменные для работы с разметкой для обработкой событий
+const refs = {
+  ulGallery: document.querySelector(".js-gallery"),
+  divLightbox: document.querySelector(".js-lightbox"),
+  imglightbox: document.querySelector("img.lightbox__image"),
+  btnLightbox: document.querySelector("button.lightbox__button"),
+};
+
+//добавление тегов разметки, используя шаблонные строки и insertAdjacentHTML().
+const elementLi = createElementImg.call(imagesObj);
+
+refs.ulGallery.insertAdjacentHTML("beforeend", elementLi);
+//Открытие модального окна по клику на элементе галереи
+refs.ulGallery.addEventListener("click", ulGalleryClick);
+//Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"]
+refs.btnLightbox.addEventListener("click", btnCloseClick);
+//Закрытие модального окна по клику на div.lightbox__overlay
+refs.divLightbox.addEventListener("click", btnCloseClick);
+//Обработка событий при нажатии клавиш Escape, ArrowLeft, ArrowRight - закрытие модального окна и пролистывание картинок
+window.addEventListener("keydown", keyboardEvent);
+
+//функция добавления разметки
+function createElementImg() {
+  return this.map(
+    (el) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href=${el.original}>
+      <img class="gallery__image" src=${el.preview}
+      data-source=${el.original} alt=${el.description}/>
+      </a>
+      </li>`
+  ).join("");
+}
+
+//функция обработки события нажатия на картинку из галереи
+function ulGalleryClick(evt) {
+  const isGallery__image = evt.target.classList.contains("gallery__image");
+  if (!isGallery__image) {
+    return;
+  }
+  addClass(); //модальное окно активно
+  evt.preventDefault();
+  refs.imglightbox.src = evt.target.dataset.source;
+  currentIndexImg = collectionImg.indexOf(refs.imglightbox.src);
+}
+//функция обработки событий от нажатия клавиш
+function keyboardEvent(evt) {
+  switch (evt.keyCode) {
+    case 27: //Закрытие модального окна по клику на Escape
+      removeClass();
+      break;
+    case 37: //при нажатии клавиши "Влево"
+      if (currentIndexImg === 0) {
+        currentIndexImg = collectionImg.length - 1;
+      } else {
+        currentIndexImg -= 1;
+      }
+      refs.imglightbox.src = collectionImg[currentIndexImg];
+      break;
+    case 39: //при нажатии клавиши "Вправо"
+      if (currentIndexImg === collectionImg.length - 1) {
+        currentIndexImg = 0;
+      } else {
+        currentIndexImg += 1;
+      }
+      refs.imglightbox.src = collectionImg[currentIndexImg];
+  }
+}
+//функция для кнопки "закрыть"
+function btnCloseClick(evt) {
+  const islightbox__image = evt.target.classList.contains("lightbox__image");
+  if (islightbox__image) {
+    return;
+  }
+  removeClass(); //модальное окно неактивно
+}
+
+//добавление класса "is-open"
+function addClass() {
+  refs.divLightbox.classList.add("is-open");
+}
+//удаление класса "is-open"
+function removeClass() {
+  refs.divLightbox.classList.remove("is-open");
+  refs.imglightbox.src = "";
+}
